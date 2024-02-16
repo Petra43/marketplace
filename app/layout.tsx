d6ec5dd33d3bfabbffc5d100e868f5c2252bb733
AnnaJamieson-create <67615414+AnnaJamieson-create@users.noreply.github.com>
2024-02-16 13:33:06 +1300

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./components/nav/NavBar";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,13 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}
+      <body className={inter.className}>
       <NavBar/>
       {children}
       <footer className="footer">
-        <p>&copy; 2024 Little Handmade Market. All rights reserved.</p>
+        
       </footer>
       </body>
+      
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-import NavBar from './components/nav/NavBar';
 
 export default function Home() {
   return (
-    <>
-    <NavBar />
  <p>hello world</p>
- </>
   );
 }
