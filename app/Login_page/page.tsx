e003761bdd3651fb6a300bedaa539f7ae08d5f33
Nxszu <nirav.nxs@gmail.com>
2024-02-19 14:48:48 +1300

    login page made with basic stylings

---
+++ app/Login_page/page.tsx
@@ -0,0 +1,75 @@
+'use client'
+import Image from "next/image";
+import styles from "./page.module.css";
+import React from "react";
+
+
+interface FormData {
+  username: string;
+  password: string;
+}
+
+export default function Home() {
+  const [formData, setFormData] = React.useState<FormData>({
+    username: "",
+    password: "",
+  });
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formDataObject: FormData = {
+      username: formData.username,
+      password: formData.password,
+    };
+
+    console.log("Submitting form data:", formDataObject);
+    // Implement email sign up logic here
+  };
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  return (
+    <div className={styles.container}>
+      <form className={styles.form} onSubmit={handleFormSubmit}>
+        <h2 className={styles.title}>Login</h2>
+        <label className={styles.label} htmlFor="Email">
+          Email:
+        </label>
+        <input
+          className={styles.input}
+          type="text"
+          id="Email"
+          name="Email"
+          value={formData.Email}
+          onChange={handleInputChange}
+          required
+        />
+
+        <label className={styles.label} htmlFor="password">
+          Password:
+        </label>
+        <input
+          className={styles.input}
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleInputChange}
+          required
+        />
+
+        <button className={styles.button} type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
