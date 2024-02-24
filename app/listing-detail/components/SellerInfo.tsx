fab7380c4c5f1598cc7433679e593ba4a316315a
smarine43 <smarine43@gmail.com>
2024-02-24 14:28:16 +1300

    created the start of the SellerInfo Component

---
+++ app/listing-detail/components/SellerInfo.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function SellerInfo({sellerId}:{sellerId: string}) {
+  return (
+    <div className="container">
+      <div className="image"></div>
+      <div className="name"></div>
+    </div>
+  )
+}
