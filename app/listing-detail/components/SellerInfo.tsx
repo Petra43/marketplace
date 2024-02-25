af540019e7c4c937398fbc28576b266bd424a851
smarine43 <smarine43@gmail.com>
2024-02-25 14:32:06 +1300

    did some styling and am working on getting an images to work

--- app/listing-detail/components/SellerInfo.tsx
+++ app/listing-detail/components/SellerInfo.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import style from "./sellerInfo.module.css"
+import Image from "next/image";
 
 export default function SellerInfo({sellerId}:{sellerId: string}) {
+  
+  const sellerName = "Petra Parker"
+  
   return (
-    <div className="container">
-      <div className="image"></div>
-      <div className="name"></div>
+    <div className={style.container}>
+      <div className={style.profileImage}>
+        <Image 
+          src="/public/assets/icons/user-3296.png"
+          alt="default profile image"
+          fill={true}
+        /> 
+      </div>
+      <div className={style.infoContainer}>{sellerName}</div>
     </div>
   )
 }
