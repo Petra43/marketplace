5b54f2a3e46fd6883e52d70fa14477e276b9c2f3
AnnaJamieson-create <67615414+AnnaJamieson-create@users.noreply.github.com>
2024-03-09 12:13:48 +1300

    commit

--- app/components/listingitem/ListingCard.tsx
+++ app/components/listingitem/ListingCard.tsx
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
 import Image from 'next/image';
 import babyknitted from '/public/babyknitted.jpg';
-import { ListingResult } from './producttypes';
 import styles from './productList.module.css';
 import { formatPrice } from './formatprice';
 
-
+/** Anna Created this Listing Card Page which styles the Listing Card Component */
 export default function ListingCard({listingResult}): React.FC<{}> {
+  const img = {width:"100%", height:"60%"} 
     return (
-      <div className={styles.grid}>
-        <div>
-          <Image src={babyknitted} width= {300} height={135} alt='knitted-baby-beanie' className={styles.listingimg} />
-          <div className={styles.card}>
-            <div className={styles.text}>
-              <h3>{listingResult.name}</h3>
-              <p>{listingResult.description}</p>
-              <p className={styles.price}>{formatPrice(listingResult.price)}</p>
-            </div>
-          </div>
+      <div className={styles.card}>
+        <Image 
+          src={babyknitted} style={img}
+          alt='knitted-baby-beanie'  
+          className={styles.listingimg} 
+          />
+        <div className={styles.text}>
+          <h2>{listingResult.name}</h2>
+          <p>{listingResult.description}</p>
+          <p className={styles.price}>{formatPrice(listingResult.price)}</p>
         </div>
       </div>
-          );
-  };
+      );
+};
--- app/components/listingitem/producttypes.ts
+++ app/components/listingitem/producttypes.ts
@@ -1,3 +1,5 @@
+/** Anna Created this type for listing results so that the information can be pulled into other pages nicely */
+
 export type ListingResult = {
     id: number;
     name: string;
