7c33e1ec22642189f9492a6d9fc4e743582eecec
AnnaJamieson-create <67615414+AnnaJamieson-create@users.noreply.github.com>
2024-03-09 14:40:47 +1300

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import bannerImg from '/public/bannerImg.png';
 import styles from "./page.module.css";
 import ListingCard from "./components/listingitem/ListingCard";
 import { ListingResult } from "./components/listingitem/productTypes";
@@ -16,12 +17,21 @@ export default function Home() {
     category: "Knitted",
     thumbnailURL: "",
   }
+  const img = {width:"100%", height:"100%"} 
 
   return (
-    <>
-     <p></p>
-     <ListingSection></ListingSection>
-    </>
-  );
+<div>
+    <div className="headerImage">
+    <Image 
+        src={bannerImg} style={img}
+        alt='clay-wheel'  
+        className={styles.headerImage}/>
+    </div>
+  <h1 className={styles.headerTitle}>Most Trusted Market Place for <br/> Hand Made Products in <br/> New Zealand </h1>
+  <div>
+  <ListingSection></ListingSection>
+  </div>
+</div>
+);
 }
 
