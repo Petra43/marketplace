f5f64ce9a48c69cb5eea3f75f948ac773ef62e66
AnnaJamieson-create <67615414+AnnaJamieson-create@users.noreply.github.com>
2024-03-15 12:13:45 +1300

    commit

--- app/components/listing-section/listingSection.tsx
+++ app/components/listing-section/listingSection.tsx
@@ -7,7 +7,7 @@ const jua=Jua({weight:"400", subsets:["latin"]})
 
 /** Anna Created this Listing Section which feeds information to the listing cards */
 
-export default function ListingSection(): React.FC<{}> {
+export default function ListingSection(): React.JSX.Element {
     const content: React.JSX.Element[] = []
     const testData: listingResult[] = listingInfo // this will be replaced with a data base call in the future
     testData.forEach( listingSummery => content.push(<ListingCard listingResult={listingSummery}/>))
--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,6 @@ import ListingSection from "./components/listing-section/listingSection";
 
 
 export default function Home() {
-  const listingResult:ListingResult = {
-    id: 2,
-    name: "Knitted Beanie",
-    description: "Veniam aliquip nisi qui adipisicing est sit mollit eiusmod aliqua nostrud irure do sint quis.",
-    price: 20.00,
-    category: "Knitted",
-    thumbnailURL: "",
-  }
   const img = {width:"100%", height:"100%"} 
 
   return (
@@ -28,7 +20,7 @@ export default function Home() {
         className={styles.headerImage}/>
     </div>
   <h1 className={styles.headerTitle}>Most Trusted Market Place for <br/> Hand Made Products in <br/> New Zealand </h1>
-  <div>
+  <div className="listingSection">
   <ListingSection></ListingSection>
   </div>
 </div>
