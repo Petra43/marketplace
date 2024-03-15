7f52f262d6143d523e25080ea8e66a8c670f1185
AnnaJamieson-create <67615414+AnnaJamieson-create@users.noreply.github.com>
2024-03-15 13:58:28 +1300

    commit

--- app/components/listingitem/ListingCard.tsx
+++ app/components/listingitem/ListingCard.tsx
@@ -3,10 +3,14 @@ import Image from 'next/image';
 import babyknitted from '/public/babyknitted.jpg';
 import styles from './productList.module.css';
 import { formatPrice } from './formatprice';
+import { ListingResult } from './productTypes';
 
 /** Anna Created this Listing Card Page which styles the Listing Card Component */
-export default function ListingCard({listingResult}): React.FC<{}> {
+export default function ListingCard({listingResult}:{listingResult:ListingResult}): React.JSX.Element {
   const img = {width:"100%", height:"60%"} 
+
+const price = listingResult.price !==undefined ? listingResult.price :0 
+
     return (
       <div className={styles.card}>
         <Image 
@@ -17,7 +21,7 @@ export default function ListingCard({listingResult}): React.FC<{}> {
         <div className={styles.text}>
           <h2>{listingResult.name}</h2>
           <p>{listingResult.description}</p>
-          <p className={styles.price}>{formatPrice(listingResult.price)}</p>
+          <p className={styles.price}>{formatPrice(price)}</p>
         </div>
       </div>
       );
