c8387fb14514a486ee40dd983684cd542d667b33
AnnaJamieson-create <67615414+AnnaJamieson-create@users.noreply.github.com>
2024-03-16 12:17:21 +1300

    commit

--- app/components/listingitem/ListingCard.tsx
+++ app/components/listingitem/ListingCard.tsx
@@ -4,6 +4,10 @@ import babyknitted from '/public/babyknitted.jpg';
 import styles from './productList.module.css';
 import { formatPrice } from './formatprice';
 import { ListingResult } from './productTypes';
+import TipTap from '../input-components/tiptapEditor';
+import { Content } from 'next/font/google';
+import { link } from 'fs';
+import Link from 'next/link';
 
 /** Anna Created this Listing Card Page which styles the Listing Card Component */
 export default function ListingCard({listingResult}:{listingResult:ListingResult}): React.JSX.Element {
@@ -12,17 +16,19 @@ export default function ListingCard({listingResult}:{listingResult:ListingResult
 const price = listingResult.price !==undefined ? listingResult.price :0 
 
     return (
-      <div className={styles.card}>
-        <Image 
-          src={babyknitted} style={img}
-          alt='knitted-baby-beanie'  
-          className={styles.listingimg} 
-          />
-        <div className={styles.text}>
-          <h2>{listingResult.name}</h2>
-          <p>{listingResult.description}</p>
-          <p className={styles.price}>{formatPrice(price)}</p>
+      <Link href={`/listing-detail/${listingResult.id}`} className={styles.link}>
+        <div className={styles.card}>
+          <Image 
+            src={babyknitted} style={img}
+            alt='knitted-baby-beanie'  
+            className={styles.listingimg} 
+            />
+          <div className={styles.text}>
+            <h2>{listingResult.name}</h2>
+            <TipTap isEdit={false}content={listingResult.description}setContent={()=>{}}></TipTap>
+            <p className={styles.price}>{formatPrice(price)}</p>
+          </div>
         </div>
-      </div>
+      </Link>
       );
 };
